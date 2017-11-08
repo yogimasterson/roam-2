7474a70ef752f5be66c9e2477fba7cf09a83db6d
Michael Masterson <michaelmasterson@Michaels-MacBook-Pro.local>
2017-11-07 17:10:53 -0800

    create database and tables, atempt migration, run into migration problems.

---
+++ migrations/sqls/20171108001048-init-tables-down.sql
@@ -0,0 +1,6 @@
+/* Replace with your SQL commands */
+
+DROP TABLE IF EXISTS cities;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS reviews;
+DROP TABLE IF EXISTS session;
---
+++ migrations/sqls/20171108001048-init-tables-up.sql
@@ -0,0 +1,31 @@
+/* Replace with your SQL commands */
+
+CREATE TABLE cities(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    encrypted_password VARCHAR(255) NOT NULL,
+    image TEXT,
+    join_date TIMESTAMP(6) WITH TIME ZONE NOT NULL,
+    cities_id INT REFERENCES cities
+);
+
+CREATE TABLE REVIEWS(
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    users_id INT REFERENCES users,
+    cities_id INT REFERENCES cities
+);
+
+CREATE TABLE "session"(
+    "sid" VARCHAR NOT NULL COLLATE "default",
+    "sess" JSON NOT NULL,
+    "expire" TIMESTAMP(6)
+)
+WITH (OIDS=FALSE);
+ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
